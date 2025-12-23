52f7058623482924ff077f716850fdbb5322543c
ibu0125 <ibu01257248@gmail.com>
2025-12-23 19:41:50 +0900

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go_project
 
 go 1.24.0
+
+require github.com/line/line-bot-sdk-go v7.8.0+incompatible 
--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
-	"encoding/json"
-	"io"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
+
+
 
 func validateSignature(body []byte, signature string, secret string) bool {
 	mac := hmac.New(sha256.New, []byte(secret))
@@ -20,6 +23,15 @@ func validateSignature(body []byte, signature string, secret string) bool {
 
 
 func main() {
+
+	LINE_CHANNEL_SECRET:=os.Getenv("LINE_CHANNEL_SECRET")
+	LINE_CHANNEL_ACCESS_TOKEN:=os.Getenv("LINE_CHANNEL_ACCESS_TOKEN")
+	bot,err:=linebot.New(LINE_CHANNEL_SECRET,LINE_CHANNEL_ACCESS_TOKEN)
+	if err !=nil{
+		fmt.Printf("",err)
+	}
+
+
 	port := os.Getenv("PORT")
 	if port=="" {
 		port="8080"
@@ -31,46 +43,32 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
-
-	http.HandleFunc("/hello",func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		res:=map[string]string{
-			"Message":"Hello API",
-			"Status":http.StatusText(http.StatusOK),
-		}
-
-		w.Header().Set("Content-Type","application/json")
-		err:=json.NewEncoder(w).Encode(res)
-		if err != nil {
-			log.Printf("Json Err: %v",err)
-		}
-	})
-
-
 	http.HandleFunc("/callback",func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
+        events, err := bot.ParseRequest(r)
+        if err != nil {
+            if err == linebot.ErrInvalidSignature {
+                w.WriteHeader(http.StatusBadRequest)
+            } else {
+                w.WriteHeader(http.StatusInternalServerError)
+            }
+            log.Printf("ParseRequest error: %v", err)
+            return
+        }
+
+		for _,ev:=range events{
+			switch ev.Type{
+				case linebot.EventTypeFollow:
+					userId:=ev.Source.UserID
+
+					if _,err:=bot.ReplyMessage(ev.ReplyToken,linebot.NewTextMessage("よろしく")).Do();err!=nil{
+						log.Printf("ReplyMessage error: %v", err)
+					}
+			}
+
+
 		}
 
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Bad Request", http.StatusBadRequest)
-			return
-		}
 
-		signature := r.Header.Get("X-Line-Signature")
-		secret := os.Getenv("LINE_CHANNEL_SECRET")
-
-		if !validateSignature(body, signature, secret) {
-			http.Error(w, "Invalid signature", http.StatusUnauthorized)
-			return
-		}
-
-		log.Println("LINE Webhook OK")
-
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
 	})
 
 }
